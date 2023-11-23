6de3d9681914e2d246da095e63676aa1ed413625
Cristian Mora <cristianfmorac@gmail.com>
2023-11-22 20:20:41 -0500

    sincronizar arch.sql

--- mover.sh
+++ mover.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 ruta=$(pwd)
+
+# crear fecha actual
 fecha=$(date +%Y-%m-%d)-db
 
-#for archivo in $(ls -la $ruta/base-datos | grep .sql | awk '{print $9}')
+# bucle para recorrer el archivo en la carpeta
 for archivo in $(ls $ruta/base-datos)
 do
+  # crear carpeta con fecha actual
   [ -d $fecha ] || mkdir $fecha
+  # sincronizar los archivos carpeta base-datos
   rsync -r $ruta/base-datos/$archivo /home/devops/$fecha/
 done
