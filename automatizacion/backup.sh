c33adb6afc3ac431507edade6720fdb1dee9af2a
Cristian Personal <cristianfmorac@gmail.com>
2025-04-09 17:46:28 -0500

    backup ficheros por extensión

--- automatizacion/backup.sh
+++ automatizacion/backup.sh
@@ -1,16 +1,35 @@
 #! /bin/bash
 
+:<<'COMENTARIO'
+Automatizacion para crear un script
+que realiza la sincronización de ficheros dependiendo la extensión
+en una carpeta llamada (fecha_actual-extension-bk)
+ejecutar script ./backup.sh [name-extension]
+COMENTARIO
+
+# Ruta absoluta
 ruta=$(pwd)
+extension=$1
 
 # crear fecha actual
-fecha=$(date +%Y-%m-%d)-db
+fecha=$(date +%Y-%m-%d)-$extension-bk
 
-# bucle para recorrer el archivo en la carpeta
-for archivo in $(ls $ruta/base-datos)
+# Crear una carpeta con la fecha actual
+:<<validacion
+-d valida si la carpeta existe
+si no existe despues de || crea la carpeta
+validacion
+
+[ -d $fecha ] || mkdir $fecha
+
+# filtrar por archivos .txt
+# grep -E para poder utilizar expresiones regulares
+for file in $(ls | grep -E "\.${extension}");
 do
-  # crear carpeta con fecha actual
-  [ -d $fecha ] || mkdir $fecha
-  # sincronizar los archivos carpeta base-datos
-  #rsync -r $ruta/base-datos/$archivo $ruta/$fecha/
-  mv $ruta/base-datos/$archivo $ruta/$fecha
+  # sincronizar los archivos en la carpeta backup
+  rsync $file $fecha
+  # eliminar el fihero
+  rm $file
 done
+
+echo ls | grep -E "\.${extension}"
